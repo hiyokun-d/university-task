bcc1db69c9906a807bb6b002cadb4d5faf1c4c39
nothing <ddwi2504@gmail.com>
2024-12-02 14:21:47 +0700

    correcting answer of d and e

--- chapter8/d.c
+++ chapter8/d.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
+FILE *fptr;
+
 int main() {
-  FILE *fptr = fopen("testdata.in", "r");
+  fptr = fopen("testdata.in", "r");
   if (fptr == NULL) {
     printf("Error opening file.\n");
     return 1;
   }
 
-  int T;
-  fscanf(fptr, "%d", &T);
+  int t;
+  fscanf(fptr, "%d", &t);
 
-  while (T--) { // Process each test case
-    char str[101];
-    fscanf(fptr, "%s", str);
+  for (int i = 0; i < t; i++) {
+    char str[110];
+    fscanf(fptr, " %[^\n]", str);
+    int len = strlen(str);
+    int counter[26] = {0};   // To count each character occurrence
+    char alphabet[26] = {0}; // To track which characters have been replaced
 
-    int N;
-    fscanf(fptr, "%d", &N);
+    int n;
+    fscanf(fptr, "%d", &n);
+    int checking[26] = {0}; // To prevent multiple replacements for same char
 
-    // Process transformations
-    for (int i = 0; i < N; i++) {
-      char from, to;
-      fscanf(fptr, " %c %c", &from, &to);
-      // Apply this transformation immediately to all matching characters
-      for (int j = 0; str[j] != '\0'; j++) {
-        if (str[j] == from) {
-          str[j] = to;
+    for (int j = 0; j < n; j++) {
+      char a, b;
+      fscanf(fptr, " %c %c", &a, &b);
+
+      if (checking[a - 'A'] == 0) {
+        for (int k = 0; k < len; k++) {
+          if (str[k] == a) {
+            str[k] = b; // Replace character in string
+          }
         }
+        checking[a - 'A'] = 1; // Mark character as replaced
       }
     }
 
-    // Count frequencies
-    int freq[26] = {0};
-    for (int i = 0; str[i] != '\0'; i++) {
-      freq[str[i] - 'A']++;
+    // Count the occurrences of each character in the modified string
+    for (int j = 0; j < len; j++) {
+      if (str[j] >= 'A' && str[j] <= 'Z') {
+        counter[str[j] - 'A']++;
+      }
     }
 
-    // Print in alphabetical order
-    for (int i = 0; i < 26; i++) {
-      if (freq[i] > 0) {
-        printf("%c %d\n", (char)('A' + i), freq[i]);
+    // Print characters in alphabetical order with their counts
+    for (int j = 0; j < 26; j++) {
+      if (counter[j] > 0) {
+        printf("%c %d\n", 'A' + j, counter[j]);
       }
     }
   }
--- chapter8/e.c
+++ chapter8/e.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  freopen("testdata.in", "r", stdin);
 
+  FILE *f = fopen("testdata.in", "r");
   int T;
-  scanf("%d", &T);
+  fscanf(f, "%d\n", &T);
 
-  for (int i = 1; i <= T; i++) {
+  for (int i = 0; i < T; i++) {
     int N;
-    scanf("%d", &N);
-
-    int maxHeight = 0;
-    int totalWidth = N * 2 + 2; // Corrected width calculation (N*2 + 2)
-    int area = 0;
-
-    for (int j = 0; j < N; j++) {
-      int cubes;
-      scanf("%d", &cubes);
-      int height = cubes * 2; // Height of stack
-      if (height > maxHeight) {
-        maxHeight = height; // Track the maximum height
+    int sideSum = 0;
+    int areaSum = 0;
+    int blockSum = 0;
+    fscanf(f, "%d\n", &N);
+    int height[N];
+    height[0] = 0;
+    for (int j = 1; j <= N; j++) {
+      fscanf(f, "%d", &height[j]);
+      blockSum = blockSum + height[j];
+      sideSum = sideSum + height[j] * 4 + 4;
+      if (height[j] >= height[j - 1]) {
+        sideSum = sideSum - height[j - 1] * 4;
+      } else {
+        sideSum = sideSum - height[j] * 4;
       }
-      area += height * 2; // Area of each stack (height * width)
     }
+    fscanf(f, "\n");
 
-    int perimeter =
-        2 * totalWidth + 2 * maxHeight; // Corrected perimeter formula
-    printf("Case #%d: %d %d\n", i, perimeter, area); // Output result
+    areaSum = blockSum * 4;
+    printf("Case #%d: %d %d\n", i + 1, sideSum, areaSum);
   }
+
+  fclose(f);
 
   return 0;
 }
